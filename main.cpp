c9d89ac74650c6b6c6308c563d8d811e93e48634
Qin <qin.shao.ca@gmail.com>
2021-02-27 03:48:07 +0800

    using vector copy function with objecr return the addresses of two object is different;

--- main.cpp
+++ main.cpp
@@ -18,6 +18,7 @@ int main(){
     sa.at(0) = 555;
     cout << sa.at(0) << endl;
     cout << sa2.at(0) << endl;
-
+    cout <<"sa memory address: " << &sa << endl;
+    cout <<"sa2 memory address: " << &sa2 << endl;
     return 0;
 }
